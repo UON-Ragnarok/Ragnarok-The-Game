0ef4915ee87b2ad7f64d2b943e3a395c4891ad43
Mike <bdanh96@gmail.com>
2017-12-04 16:58:54 +0000

    Main menu changes

--- Main.py
+++ Main.py
@@ -23,9 +23,12 @@ pause_time = 0
 pause_start_time=time.time()
 alive = True
 pause = False
-intro = True
+flag = True
+background_y = 0
 
 background = pygame.image.load('img/background.jpg').convert()
+menu_background = pygame.image.load('img/main_menu_bg.jpg').convert()
+title = pygame.image.load('img/Ragnarok_logo.png').convert_alpha()
 ship_image = pygame.image.load('img/spaceship.png').convert()
 boss_image = pygame.image.load('img/thor.png').convert()
 enemy_image = pygame.image.load('img/mob.png').convert()
@@ -34,7 +37,7 @@ meteor_image = pygame.image.load('img/meteor.png').convert()
 start_button_image = pygame.image.load('img/start_button.png').convert()
 about_button_image = pygame.image.load('img/about_button.png').convert()
 
-background_y = 0
+
 pygame.display.set_caption("My Game")
 
 #List of all sprites
@@ -56,8 +59,7 @@ mob_list = pygame.sprite.Group()
 meteor_list = pygame.sprite.Group()
 
 #Creating sprites
-player = PlayerShip(screen_width, ship_image)
-sprites_list.add(player)
+player = PlayerShip(screen_width, screen_height,ship_image, [sprites_list])
 
 clock = pygame.time.Clock()
 FPS = 120
@@ -67,12 +69,20 @@ fire_bullet_event = pygame.USEREVENT + 1
 fire_bullet_delay = 300
 pygame.time.set_timer(fire_bullet_event, 500)
 
-player.rect.y = (screen_height - player.rect.height)*0.95
-
 pygame.mixer.Channel(0).play(pygame.mixer.Sound('Arcade Funk.ogg'))
 pygame.mixer.Channel(0).set_volume(0.5)
-#Spawning enemies
 
+# main menu
+# set up the height and width
+sb_top_left_x = screen_width/2 - start_button_image.get_rect().width/2
+sb_top_left_y = screen_height/2
+sb_height = start_button_image.get_rect().height
+sb_width = start_button_image.get_rect().width
+ab_height = about_button_image.get_rect().height
+ab_width = about_button_image.get_rect().width
+about = False
+
+#Spawning enemies
 def spawn_enemy(speed):
     for i in range (6):
         enemy = Enemy(enemy_image, speed, 0, [enemy_list, mob_list, sprites_list])
@@ -99,68 +109,65 @@ def fire_bullet():
     pygame.time.set_timer(fire_bullet_event, fire_bullet_delay)
 
 
-# main menu
-# set up the height and width
-sb_top_left_x=screen_width/2 - start_button_image.get_rect().width/2
-sb_top_left_y=screen_height/2
-sb_height=start_button_image.get_rect().height
-sb_width=start_button_image.get_rect().width
-ab_height=about_button_image.get_rect().height
-ab_width=about_button_image.get_rect().width
-about=False 
+def intro():
+    menu_background_x = 0
+    while True:
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        click = pygame.mouse.get_pressed()
+        mouse = pygame.mouse.get_pos()
 
-while intro:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+        # --- Looping the background
+        relative_x = menu_background_x % menu_background.get_rect().width
+        screen.blit(menu_background, [relative_x - menu_background.get_rect().width, 0])
+        if relative_x < screen_width:
+            screen.blit(menu_background, [relative_x, 0])
+        menu_background_x += -0.3
 
-    click = pygame.mouse.get_pressed()
-    mouse = pygame.mouse.get_pos()
-    # start button
-    if sb_top_left_x <mouse[0]<sb_top_left_x+sb_width and sb_top_left_y <mouse[1]<sb_top_left_y +sb_height:
-        screen.fill((0,0,0))
-        screen.blit(pygame.font.SysFont("'freesansbold.ttf'", 70, True).render("Ragnorak", 1, (91, 109, 131)), (sb_top_left_x-20,sb_top_left_y -20-sb_height))
-        big_start_button_image=pygame.transform.rotozoom(start_button_image,0,1.2)
-        screen.blit(big_start_button_image, [sb_top_left_x, sb_top_left_y])
-        screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
-        pygame.display.flip()
-        if click[0] == 1:
-            intro = False
-            break
-    # about button
-    elif sb_top_left_x <mouse[0]<sb_top_left_x+ab_width and sb_top_left_y +20+ab_height<mouse[1]<sb_top_left_y+20+sb_height+ab_height:
-        screen.fill((0,0,0))
-        screen.blit(pygame.font.SysFont("'freesansbold.ttf'", 70, True).render("Ragnorak", 1, (91, 109, 131)), (sb_top_left_x-20,sb_top_left_y -20-sb_height))
-        big_about_button_image=pygame.transform.rotozoom(about_button_image,0,1.2)
-        screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y])
-        screen.blit(big_about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
-        pygame.display.flip()
-        #if click[0]==1:
-            #screen.fill((0,0,0))
-            #screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y]);
-            #screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
-            #pygame.display.flip()
-            #while about ==True:
-                #if sb_top_left_x <mouse[0]<sb_top_left_x+ab_width and sb_top_left_y +20+sb_height<mouse[1]<sb_top_left_y+20+sb_height+ab_height:
-                    #screen.fill((0,0,0))
-                    #big_about_button_image=pg.transform.rotozoom(about_button_image,0,1.2)
-                    #screen.blit(big_about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height]);
-                    #pygame.display.flip()
-                    #if click[0] == 1:
-                       # continue
-               # else:
-                    #screen.fill((0,0,0))
-                    #screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y]);
-                    #screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
-    else:
-        screen.fill((0,0,0))
-        screen.blit(pygame.font.SysFont("'freesansbold.ttf'", 70, True).render("Ragnorak", 1, (91, 109, 131)), (sb_top_left_x-20,sb_top_left_y -20-sb_height))
-        screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y])
-        screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
-        pygame.display.flip()
+        screen.blit(title, [screen_width/6, screen_height/ 5])
 
+        # start button
+        if sb_top_left_x < mouse[0] < sb_top_left_x+sb_width and sb_top_left_y < mouse[1] < sb_top_left_y + sb_height:
+            big_start_button_image = pygame.transform.rotozoom(start_button_image,0,1.2)
+            screen.blit(big_start_button_image, [sb_top_left_x, sb_top_left_y])
+            screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
+            pygame.display.flip()
+            if click[0] == 1:
+                break
+        # about button
+        elif sb_top_left_x < mouse[0] < sb_top_left_x+ab_width and sb_top_left_y + 20 + ab_height < mouse[1] < sb_top_left_y + 20 + sb_height+ab_height:
+            big_about_button_image = pygame.transform.rotozoom(about_button_image,0,1.2)
+            screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y])
+            screen.blit(big_about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
+            pygame.display.flip()
+            #if click[0]==1:
+                #screen.fill((0,0,0))
+                #screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y]);
+                #screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
+                #pygame.display.flip()
+                #while about ==True:
+                    #if sb_top_left_x <mouse[0]<sb_top_left_x+ab_width and sb_top_left_y +20+sb_height<mouse[1]<sb_top_left_y+20+sb_height+ab_height:
+                        #screen.fill((0,0,0))
+                        #big_about_button_image=pg.transform.rotozoom(about_button_image,0,1.2)
+                        #screen.blit(big_about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height]);
+                        #pygame.display.flip()
+                        #if click[0] == 1:
+                           # continue
+                   # else:
+                        #screen.fill((0,0,0))
+                        #screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y]);
+                        #screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
+        else:
+            screen.blit(start_button_image, [sb_top_left_x, sb_top_left_y])
+            screen.blit(about_button_image, [sb_top_left_x, sb_top_left_y + 20 + sb_height])
+            pygame.display.flip()
+
+        clock.tick(FPS)
+
+intro()
 done = False
 
 # -------- Main Program Loop -----------
@@ -186,6 +193,8 @@ while not done:
             if event.key == pygame.K_r and (not alive):
                 score = 0
                 alive = True
+                intro()
+                player = PlayerShip(screen_width,screen_height,ship_image, [sprites_list])
             if event.key == pygame.K_n and (not alive):
                 done = True
             if event.key == pygame.K_ESCAPE and alive:
@@ -206,7 +215,7 @@ while not done:
                     pause = False
                     player.pause = False
                     pause_time += time.time() - pause_start_time
-                    
+
     sprites_list.update()
     # --- Game mechanics
 
@@ -217,14 +226,14 @@ while not done:
             alive= False
             pygame.mixer.Channel(4).play(pygame.mixer.Sound('killed.ogg'))
 
-             
+
         for bullet in bullet_list:
             enemies_hit_list = pygame.sprite.spritecollide(bullet, mob_list, True)
             for enemies in enemies_hit_list:
                 score += 1
                 bullet.kill()
                 pygame.mixer.Channel(3).play(pygame.mixer.Sound('explo.ogg'))
-                pygame.mixer.Channel(3).set_volume(0.5)               
+                pygame.mixer.Channel(3).set_volume(0.5)
 
             #if bullet goes off screen
             if bullet.rect.y < -10:
@@ -256,11 +265,10 @@ while not done:
             spawn_boss(0)
 
     if not alive:
-       # sprites_list.remove(player)
-        screen.fill((0,0,0))
+       # sprites_list.remove(player))
         Menu().displayMenu(screen,"c",score)
         current_level=0
-        for sprite in enemy_list:
+        for sprite in sprites_list:
             sprite.kill()
     elif pause :
         Menu().displayMenu(screen, "b")
--- PlayerShip.py
+++ PlayerShip.py
@@ -2,11 +2,13 @@ import pygame
 
 class PlayerShip(pygame.sprite.Sprite):
 
-    def __init__(self, screen_width, image_location):
-        super().__init__()
+    def __init__(self, screen_width, screen_height, image_location, *group):
+        super().__init__(*group)
         self.image = image_location
-        self.screen = screen_width
+        self.screen_width = screen_width
+        self.screen_height = screen_height
         self.rect = self.image.get_rect()
+        self.rect.y = (self.screen_height - self.rect.y)*0.95
         self.image.set_colorkey([255,255,255])          #make bg transparent
         self.pause = False
 
@@ -14,8 +16,8 @@ class PlayerShip(pygame.sprite.Sprite):
         pos = pygame.mouse.get_pos()
         if self.pause == False:
             self.rect.x = pos[0]
-            if pos[0] > (self.screen - self.image.get_rect().width):
-                self.rect.x = (self.screen - self.image.get_rect().width)
+            if pos[0] > (self.screen_width - self.image.get_rect().width):
+                self.rect.x = (self.screen_width - self.image.get_rect().width)
 
 
 
