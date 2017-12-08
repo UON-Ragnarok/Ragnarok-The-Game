bf491d31cd3a5ea13c33804131981e0fdfbef0dc
Gloriayoyoyo <gaoleyang@126.com>
2017-12-08 03:10:23 +0000

    resend

--- Bullet.py
+++ Bullet.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos, speed, *groups):
+    def __init__(self, x_pos, y_pos, speed, *groups, forward):
         super().__init__(*groups)
         self.image = pygame.Surface([5,10])
         self.image.fill([255,255,255]) #black bullet, place holder, need to find image or something
@@ -9,6 +9,11 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.x  = x_pos
         self.rect.y  = y_pos
         self.speed = speed
-
+        self.forward = forward
+        
     def update(self):
-        self.rect.y -= self.speed
+
+        if self.forward:
+            self.rect.y -= self.speed
+        else:
+            self.rect.y += self.speed
