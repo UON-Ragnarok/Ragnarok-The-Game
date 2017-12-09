27ed1fed432121004ab1f0f54e45ab7744fae43a
cyw500 <cyw500@gmail.com>
2017-12-09 13:08:52 +0000

    New game main frame

---
+++ settings.py
@@ -0,0 +1,20 @@
+# game options and settings
+
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+screen_width = 500
+screen_height = 800
+FPS = 120
+TITLE = "Ragnarok The Game"
+BGCOLOR = DARKGREY
+
+#relative_y = background_y % background.get_rect().height
+#screen.blit(background, [0, relative_y - background.get_rect().height])
