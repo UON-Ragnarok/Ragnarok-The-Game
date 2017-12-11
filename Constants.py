2dc7a01014cf69987a4990097152069dcdc6b5f2
frederic-wong <frederic-wong@users.noreply.github.com>
2017-12-11 22:08:49 +0000

    butt stuffing

--- Constants.py
+++ Constants.py
@@ -15,18 +15,6 @@ GREY = (91, 109, 131)
 BLACK = (0, 0, 0)
 YELLOW = (240, 255, 0)
 
-#Music
-pygame.mixer.set_num_channels(15)
-
-ARCADE_FUNK = 'Sound/Arcade Funk.ogg'  # Channel 0
-EXPLOSION = pygame.mixer.Sound('Sound/explo.ogg') # Channel 1
-COIN = pygame.mixer.Sound('Sound/coin.ogg')  # Channel 2
-KILLED = pygame.mixer.Sound('Sound/killed_explo.ogg')  # Channel 3
-COMET = pygame.mixer.Sound('Sound/comet.ogg')  # Channel 4
-LASER = pygame.mixer.Sound('Sound/laser.ogg')  # Channel 5
-BOSS_LASER = pygame.mixer.Sound('Sound/Boss_laser.ogg')  # Channel 6
-
-
 #Gameplay Properties
 DIFFICULTY = 10
 METEOR_SPAWN_RATE = 4
