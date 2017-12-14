a8cbcbfb4b1fe3f43cd72e98d6b800b1bfb652b3
cyw500 <cyw500@gmail.com>
2017-12-14 15:30:20 +0000

    cut more stuff

--- Intro.py
+++ Intro.py
@@ -25,7 +25,7 @@ class Intro:
         self.ss_top_left_x, self.ss_top_left_y = 50, 50  #player ships
 
         # height of image of each buttons
-        self.sb_height = self.IMGS["start_button_img"].get_rect().height  
+        self.sb_height = self.IMGS["start_button_img"].get_rect().height
         self.ss_height = self.IMGS["player_ship_img"].get_rect().height
         self.mo_height = self.IMGS["mob_img"].get_rect().height
         self.mt_height = self.IMGS["meteor_img"].get_rect().height
@@ -44,7 +44,7 @@ class Intro:
             if main:
                 # load main menu
                 self.main_menu()
-                
+
                 if self.IMGS["start_button_img"].get_rect(topleft=(self.sb_top_left_x, self.sb_top_left_y)).collidepoint(self.click_x, self.click_y):
                     # If clicked the start button, exit this loop and enter the game
                     pygame.time.wait(100)
@@ -75,27 +75,28 @@ class Intro:
     def about(self):
         # draw and write a bunch of stuff
         self.draw_img(self.IMGS["player_ship_img"], self.ss_top_left_x, self.ss_top_left_y)
-        self.draw_text(20, YELLOW, "Use mouse or trackpad to control", (190, 75))
-        self.draw_text(20, YELLOW, "the spaceship and destroy Asgard", (190, 85))
+        self.draw_text(25, WHITE, "Use mouse or trackpad to control", (170, 75))
+        self.draw_text(25, WHITE, "the spaceship and destroy Asgard", (170, 95))
         self.draw_img(self.IMGS["mob_img"], self.ss_top_left_x, self.ss_top_left_y + 25 + self.ss_height)
-        self.draw_text(20, YELLOW, "Sworn protectors of Asgard,", (190, 175))
-        self.draw_text(20, YELLOW, "destroy them to earn points", (190, 185))
+        self.draw_text(25, WHITE, "Sworn protectors of Asgard,", (170, 175))
+        self.draw_text(25, WHITE, "destroy them to earn points", (170, 195))
         self.draw_img(self.IMGS["meteor_img"], self.ss_top_left_x - 10, self.ss_top_left_y + 25 + self.ss_height + 25 + self.mo_height)
-        self.draw_text(20, YELLOW, "Meteorites are indestructable,", (190, 290))
-        self.draw_text(20, YELLOW, "avoid them at all cost", (190, 300))
+        self.draw_text(25, WHITE, "Meteorites are indestructable,", (170, 290))
+        self.draw_text(25, WHITE, "avoid them at all cost", (170, 310))
         self.draw_img(self.IMGS["powerup_img"], self.ss_top_left_x, self.ss_top_left_y + 25 + self.ss_height + 25 + self.mo_height + 25 + self.mt_height)
-        self.draw_text(20, YELLOW, "Collect to gain awesome powers", (190, 410))
-        self.draw_img(self.IMGS["thor_img"], self.ss_top_left_x - 50, self.ss_top_left_y + 25 + self.ss_height + 25 + self.mo_height + 25 + self.mt_height + 25 + self.pw_height)
-        self.draw_text(60, YELLOW, "???", (190, 590))
+        self.draw_text(25, WHITE, "Collect to gain awesome powers", (170, 410))
+        self.draw_img(self.IMGS["thor_img"], self.ss_top_left_x - 40, self.ss_top_left_y + 25 + self.ss_height + 25 + self.mo_height + 25 + self.mt_height + 25 + self.pw_height)
+        self.draw_text(60, WHITE, "???", (190, 590))
         self.draw_img(self.IMGS["back_button_img"], self.bb_top_left_x, self.sb_top_left_y + 300)
         self.mouse_over_enlarge(self.IMGS["back_button_img"], self.bb_top_left_x, self. sb_top_left_y + 300)
 
-    def mouse_over_enlarge(self, img_in, pos_x, pos_y): # enlarge button image when on hover
+    def mouse_over_enlarge(self, img_in, pos_x, pos_y):  # enlarge button image when on hover
         if img_in.get_rect(topleft=(pos_x, pos_y)).collidepoint(pygame.mouse.get_pos()):
             self.big_img_in = pygame.transform.rotozoom(img_in, 0, 1.2)
             self.draw_img(self.big_img_in, pos_x, pos_y)
 
-    def click_event(self): # get mouse events
+
+    def click_event(self):  # get mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -106,7 +107,7 @@ class Intro:
                     self.click_x, self.click_y = event.pos
 
     def bg_music(self):
-        # update mute button 
+        # update mute button
         if self.game.is_mute:
             self.on_off = 1
         else:
@@ -119,7 +120,7 @@ class Intro:
             self.game.is_mute = not self.game.is_mute
             self.game.set_volume(not self.game.is_mute)
             self.click_x, self.click_y = 0, 0  # reset click position, should actually really do for all(after the click start / about/ back) buttons click
-    
+
     def draw_img(self, img, x, y):
         self.game.screen.blit(img, (x, y))
 
