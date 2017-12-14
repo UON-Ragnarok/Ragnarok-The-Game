9a891d3aa969c7abcf7a0e529018ccaa4738a8c9
Gloriayoyoyo <gaoleyang@126.com>
2017-12-14 12:17:47 +0000

    comments in bosses.py

--- Bosses.py
+++ Bosses.py
@@ -5,12 +5,14 @@ from os import path
 from Constants import *
 
 class Boss(pygame.sprite.Sprite):
-
-    forward = True
-    moving_vertical = False
-    moving_horizontal = True
+    # flags for boss states
+    forward = True # whether the boss moves forward or backward horizontally
+    moving_vertical = False # whether the boss moves up or down
+    moving_horizontal = True 
+    # initialise Boss by accepting relevant data from outside of the class 
     def __init__(self, game, boss_id, screen, speed, current_level, images, *groups):
         super().__init__(*groups)
+
         self.boss_id = boss_id
         self.game = game
         self.screen = screen
@@ -22,6 +24,7 @@ class Boss(pygame.sprite.Sprite):
         self.moving_horizontal_old_time = time.time()
         self.action_old_time = time.time()
 
+        # initialise boss properties 
         self.speed = speed
         self.health = 5 * current_level
         self.total_health = self.health
@@ -40,12 +43,14 @@ class Boss(pygame.sprite.Sprite):
         self.anger_value = 0.5
         self.anger_multiplier = 1.5
 
-    # update the bossz
+    # update the boss
     def update(self):
         if self.pause == False:
-            self.say_phrases()
-            if self.health < (self.total_health / 100 * 50) and self.index < 19:
+            self.say_phrases() # boss speech
+            # boss animation
+            if self.health < (self.total_health / 100 * 50) and self.index < 19: 
                 self.update_animation()
+            
             if self.death and self.index < 30:
                 self.animation_frames = 10
                 self.update_animation()
@@ -58,6 +63,7 @@ class Boss(pygame.sprite.Sprite):
                     self.going_in = True
             else:
                 if not self.death:
+                    # define boss's movements
                     if self.boss_id == 1:
                         self.boss_one_movement()
                         self.boss_action(self.boss_id, 2, 3 * self.speed)
@@ -66,10 +72,8 @@ class Boss(pygame.sprite.Sprite):
                     elif self.boss_id == 3:
                         self.boss_one_movement()
                         self.boss_action(self.boss_id, 2, 5 * self.speed)
-        else:
-            self.rect.x += 0
-            self.current_frame += 0
 
+    # boss moves forwards and backwards horizontally, speed increases when anger
     def boss_one_movement(self):
         if self.forward:
             if self.anger:
@@ -81,67 +85,77 @@ class Boss(pygame.sprite.Sprite):
                 self.rect.x -= self.speed
             else:
                 self.rect.x -= self.speed / 2
-                # if the boss go out of the screen
+        # revert direction when reaching screen bounds
         if self.rect.right > SCREEN_WIDTH - 50 :
             self.forward = False
         if self.rect.left < 50:
             self.forward = True
 
     def boss_two_movement(self):
+        # if the boss is moving horizontally
         if self.moving_horizontal:
             if self.forward:
                 self.rect.x += self.speed
             elif not self.forward:
                 self.rect.x -= self.speed / 2
-                    # if the boss go out of the screen
+            # revert horizontal direction when reaching screen bounds
             if self.rect.right > SCREEN_WIDTH - 50 :
                 self.forward = False
             if self.rect.left < 50:
                 self.forward = True
+            # stop horizontal movement after every 3-5 seconds
             if time.time() - self.moving_horizontal_old_time > random.randint(3,5):
                 self.moving_horizontal = False
+        # otherwise
         else:
+            # moving quickly towards player
             if self.moving_vertical:
                 if self.anger:
                     self.rect.y -= self.speed * 8
                 else:
                     self.rect.y -= self.speed * 4
+            # moving towards top of screen, i.e return to top
             elif not self.moving_vertical:
                 if self.anger:
                     self.rect.y += self.speed * 8
                 else:
                     self.rect.y += self.speed * 4
+            # revert vertical direction when reaching bottom of screen
             if self.rect.bottom > (SCREEN_HEIGHT - 50) or self.rect.y < 50:
                 self.moving_vertical = not self.moving_vertical
+                # reable horizontal movement when returned to top
                 if self.rect.y < 50:
                     self.moving_horizontal_old_time = time.time()
                     self.moving_horizontal = True
-
+    # define boss's actions
     def boss_action(self, boss_id, time_between, speed):
+        # set interval between actions
         if self.anger:
             time_speed_of_actions = [time_between / 2, speed * self.anger_multiplier]
         else:
             time_speed_of_actions = [time_between , speed * self.anger_multiplier]
+        # define actions for different bosses
         if time.time() - self.action_old_time > time_speed_of_actions[0]:
-            if boss_id == 1:
+            if boss_id == 1: # boss 1 fires bullets
                 self.game.boss_fire_bullet(self, time_speed_of_actions[1])
-            if boss_id == 3:
+            if boss_id == 3: # boss 3 spawns meteors
                 self.game.spawn_meteor(time_speed_of_actions[1])
             self.action_old_time = time.time()
-
+    
     def update_health_bar(self):
         if self.going_in and not self.death:
+            # draw health bar 
             pygame.draw.line(self.screen, RED, (self.rect.x + 10, self.rect.y - 10) , (self.rect.right - 10, self.rect.y - 10), 8)
             pygame.draw.line(self.screen, GREEN, (self.rect.x + 10, self.rect.y - 10) , (self.rect.x + (self.image.get_rect().width - 10) * (self.health/ self.total_health), self.rect.y - 10), 8)
-
+    
     def update_animation(self):
         self.current_frame += 1
         if self.current_frame >= self.animation_frames:
             self.current_frame = 0
             self.index = (self.index + 1) % len(self.images)
             self.image = self.images[self.index]
-
-    def say_phrases(self):
+    
+    def say_phrases(self): # boss speech
         phrase = random.randint(1, 4)
         if not self.going_in:
             if phrase == 1:
@@ -173,13 +187,14 @@ class Boss(pygame.sprite.Sprite):
                 pygame.mixer.Channel(7).play(pygame.mixer.Sound(path.join(self.game.snd_folder, 'how can you pickle my brain.ogg')))
             self.death_speech = True
 
-    # if hit boss health -1
+    # if hit, decrease boss health
     def is_hit(self, bullet_damage):
         if self.going_in:
             self.health -= 1 + bullet_damage
+            # boss anger when health below threshold
             if self.health < self.total_health * self.anger_value:
                 self.anger = True
-
+    # returns if boss is alive
     def is_alive(self):
         return self.health > 0
 
@@ -201,10 +216,11 @@ class Boss_Bullet(pygame.sprite.Sprite):
         self.pause = False
 
 
-    # different bullets with different bosses
-    def update(self):
 
+    def update(self):
+        
         if self.pause == False:
+            # update bullet trajectory (splitting bullets)
             self.rect.x += ((self.origin_pos_x - self.boss_origin_pos_x)/25)
             self.rect.y += self.speed
 
