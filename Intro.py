a6ebaa89ed0481bd5d49dd9c21f6b7c0a339b9a3
Gloriayoyoyo <gaoleyang@126.com>
2017-12-14 12:25:49 +0000

    intro comments

--- Intro.py
+++ Intro.py
@@ -16,6 +16,7 @@ class Intro:
         self.click_x = 0  # to store the x_pos of click
         self.click_y = 0  # to store the y_pos of click
 
+        # position of buttons
         self.menu_background_img_x = 0
         self.sb_top_left_x = SCREEN_WIDTH / 2 - self.IMGS["start_button_img"].get_rect().centerx
         self.sb_top_left_y = SCREEN_HEIGHT / 2
@@ -23,7 +24,8 @@ class Intro:
         self.mb_top_left_x, self.mb_top_left_y = 130, 620
         self.ss_top_left_x, self.ss_top_left_y = 50, 50  #player ships
 
-        self.sb_height = self.IMGS["start_button_img"].get_rect().height  # start/about button image height
+        # height of image of each buttons
+        self.sb_height = self.IMGS["start_button_img"].get_rect().height  
         self.ss_height = self.IMGS["player_ship_img"].get_rect().height
         self.mo_height = self.IMGS["mob_img"].get_rect().height
         self.mt_height = self.IMGS["meteor_img"].get_rect().height
@@ -33,14 +35,16 @@ class Intro:
         about = False
         while True:
             self.click_event()
+            # rolling background
             relative_x = self.menu_background_img_x % self.IMGS["menu_background_img"].get_rect().width
             self.draw_img(self.IMGS["menu_background_img"], relative_x - self.IMGS["menu_background_img"].get_rect().width, 0)
             if relative_x < SCREEN_WIDTH:
                 self.draw_img(self.IMGS["menu_background_img"], relative_x, 0)
             self.menu_background_img_x += -0.2
             if main:
-                # It will start off loading the main page
+                # load main menu
                 self.main_menu()
+                
                 if self.IMGS["start_button_img"].get_rect(topleft=(self.sb_top_left_x, self.sb_top_left_y)).collidepoint(self.click_x, self.click_y):
                     # If clicked the start button, exit this loop and enter the game
                     pygame.time.wait(100)
@@ -86,12 +90,12 @@ class Intro:
         self.draw_img(self.IMGS["back_button_img"], self.bb_top_left_x, self.sb_top_left_y + 300)
         self.mouse_over_enlarge(self.IMGS["back_button_img"], self.bb_top_left_x, self. sb_top_left_y + 300)
 
-    def mouse_over_enlarge(self, img_in, pos_x, pos_y):
+    def mouse_over_enlarge(self, img_in, pos_x, pos_y): # enlarge button image when on hover
         if img_in.get_rect(topleft=(pos_x, pos_y)).collidepoint(pygame.mouse.get_pos()):
             self.big_img_in = pygame.transform.rotozoom(img_in, 0, 1.2)
             self.draw_img(self.big_img_in, pos_x, pos_y)
 
-    def click_event(self):
+    def click_event(self): # get mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -102,6 +106,7 @@ class Intro:
                     self.click_x, self.click_y = event.pos
 
     def bg_music(self):
+        # update mute button 
         if self.game.is_mute:
             self.on_off = 1
         else:
@@ -114,7 +119,7 @@ class Intro:
             self.game.is_mute = not self.game.is_mute
             self.game.set_volume(not self.game.is_mute)
             self.click_x, self.click_y = 0, 0  # reset click position, should actually really do for all(after the click start / about/ back) buttons click
-
+    
     def draw_img(self, img, x, y):
         self.game.screen.blit(img, (x, y))
 
